93c89a0bb3c93d7a1f2f45a261815a0a3fe24c58
ziiDeziL <44616193+ziiDeziL@users.noreply.github.com>
2018-10-30 20:38:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on("ready", () => {
 let channel =     client.channels.get("505428950881927188")
 setInterval(function() {
 channel.send(`see`);
 }, 25)
 })
-  }
-});
 
 client.login(process.env.BOT_TOKEN);
